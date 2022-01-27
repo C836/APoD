e856d9f6aec3be65c1be2dc5f4a5a0f4f5eb5bc4
C836 <gl.sepiol@gmail.com>
2022-01-27 16:24:22 -0300

    suporte de vídeos, data e media query

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
-/*copyright/ data / explanation / hdurl / title/ url*/
-window.onload = function pegar(){
-    $.ajax({url:"https://api.nasa.gov/planetary/apod?api_key=Gu0MZYR5dc4VHP5VISzp9eQINX6AibiJsP8BR9Ai",
+$("#data").datepicker({
+    onSelect: function(dateText) {
+    arr=document.getElementById('data').value.split("/");
+    data=`${arr[2]}-${arr[0]}-${arr[1]}`;
+    pegar()
+    }
+});
+
+$("#data").datepicker("setDate", new Date());
+
+arr=document.getElementById('data').value.split("/");
+data=`${arr[2]}-${arr[0]}-${arr[1]}`;
+
+function expand(){
+    imagem=document.getElementById("imagem")
+    if(window.matchMedia("(min-width:800px)").matches){
+    if(imagem.classList.contains("peq")){
+        imagem.style.cssText="width:100%; margin-bottom:3%"
+        imagem.classList.remove("peq")
+    } else {
+        imagem.style.cssText="width:50%; margin-bottom:0%"
+        imagem.classList.add("peq")}}
+}
+
+window.onload = pegar()
+function pegar(){
+    $.ajax({url:`https://api.nasa.gov/planetary/apod?api_key=Gu0MZYR5dc4VHP5VISzp9eQINX6AibiJsP8BR9Ai&date=${data}`,
         success: function(param) {
-            $('#imagem').attr('src', param.hdurl);
-            $('#background').attr('src', param.url);
+        var img=param.url
+        var hdimg=param.hdurl
+            $('#imagem').attr('src', hdimg);
+            $('#background').attr('src', img);
             $('#nome').text(param.title);
             $('#explanation').text(param.explanation);
             $('#copyName').text(param.copyright);
-        }})}
-
-$(function() {
-    $("#data").datepicker("setDate", new Date());
-});
-
+            
+        if (param.media_type==="video"){
+            console.log(param.url)
+            document.getElementById('imagem').style.cssText="display:none"
+            document.getElementById('video').style.cssText="display:block"
+            document.getElementById('video').src=param.url;
+            document.getElementById('background').src="https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569_960_720.jpg"
+        }}})}
